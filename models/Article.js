4b3abe31629f0c8ab2538f5e75051f0f3748a15a
Roman <romansenin15@gmail.com>
2019-07-22 12:38:23 -0700

    added Article model

---
+++ models/Article.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ArticleSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Comment"
+    }
+  ]
+});
+
+const Article = mongoose.model("Article", ArticleSchema);
+
+// Export the Article model
+module.exports = Article;
