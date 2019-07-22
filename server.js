668ea5990daf8cd500cf0507d518eb7e463148d4
Roman <romansenin15@gmail.com>
2019-07-22 14:29:27 -0700

    added root route

--- server.js
+++ server.js
@@ -20,11 +20,17 @@ app.use(express.static("public"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+app.get("/", function(req, res) {
+  res.render("index", {
+    msg: "Welcome!"
+  });
+});
+
 // A GET route for scraping the New York Times website
 app.get("/scrape", function(req, res) {
   axios.get("http://www.nytimes.com/").then(function(response) {
     const $ = cheerio.load(response.data);
-    
+
     $("article").each(function(i, element) {
       const result = {};
 
@@ -83,7 +89,11 @@ app.post("/articles/:id", function(req, res) {
       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      return db.Article.findOneAndUpdate(
+        { _id: req.params.id },
+        { note: dbNote._id },
+        { new: true }
+      );
     })
     .then(function(dbArticle) {
       // If we were able to successfully update an Article, send it back to the client
