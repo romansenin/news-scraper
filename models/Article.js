a693e9d96310297cc61b025153642bcc28e537de
Roman <romansenin15@gmail.com>
2019-07-23 20:45:15 -0700

    added summary field

--- models/Article.js
+++ models/Article.js
@@ -11,6 +11,10 @@ const ArticleSchema = new Schema({
     type: String,
     required: true
   },
+  summary: {
+    type: String,
+    required: true
+  },
   comments: [
     {
       type: Schema.Types.ObjectId,
